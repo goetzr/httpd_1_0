8da47e0041f959a1623783ecb956e2be90dd3ee0
Russ Goetz <russgoetz@gmail.com>
2023-01-18 20:25:04 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
+use std::net::{TcpListener, TcpStream, Ipv4Addr, SocketAddrV4};
+
+use clap::Parser;
+
 mod net;
 mod message;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser)]
+#[command(about)]
+struct CliArgs {
+    /// The HTTP server port.
+    #[arg(verbatim_doc_comment, default_value_t = 8080)]
+    port: u16,
 }
 
-fn create_socket()
+fn main() {
+    let args = CliArgs::parse();
+    let server_sock = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, args.port))?;
+
+    loop {
+        let client = 
+    }
+
+    println!("Hello, world!");
+}
 
 
